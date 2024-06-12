debc580677d3a42b73c7c423d79d50ef2f3f57f3
rlingwood <rl2036@bath.ac.uk>
2024-06-12 15:44:27 +0100

    Create gaussianfit.py

---
+++ gaussianfit.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.nddata.utils import Cutout2D
+from astropy.visualization import (SqrtStretch,
+                                   ImageNormalize)
+
+from pathlib import Path
+
+from scipy.optimize import curve_fit
+
+#################################################################
+
+
+datadir = Path("C:/Users/rubyl/Downloads/Ruby Lingwood/Data")
+
+outdir = Path("C:/Users/rubyl/Downloads/Ruby Lingwood/Data")
+
+pixsize = 4.0
+
+
+################################################################
+
+# Start plotting
+
+dset = datadir / 'afgl961_26nov_cat_mas.FIT'
+
+# Get map
+
+iext = datadir / 'afgl961_26nov_iext_mJysqa.fits'
+mapfile = fits.open(iext)
+im = np.squeeze(mapfile[0].data) # drop shallow 3rd axis from data map
+var = np.squeeze(mapfile[1].data) # drop shallow 3rd axis from variance map
+
+# get World Coordinate System info
+wcs = WCS(mapfile[0].header)
+wcs = wcs.dropaxis(2) # drop shallow 3rd axis
+
+x = np.linspace(0, 54, 54)
+y = np.linspace(0, 53, 53)
+x, y = np.meshgrid(x, y)
+
+
+# Crop to central part
+
+crop = Cutout2D(im, (round(0.5*im.shape[0]), round(0.5*im.shape[1])),
+                (0.2*im.shape[1],0.2*im.shape[0]), wcs=wcs)
+    
+cropwcs = crop.wcs
+cropim = crop.data
+
+# Plot results
+
+fig = plt.figure(figsize=(4.5,4.5))
+#ax = plt.subplot(projection=cropwcs)
+ax = plt.subplot()
+
+cropim = np.nan_to_num(cropim)
+
+norm = ImageNormalize(cropim, vmin=0., vmax=np.nanmax(cropim),
+                      stretch=SqrtStretch())    
+    
+image = plt.imshow(cropim, origin='lower', cmap=cm.Greys, norm=norm)
+        
+plt.xlabel('Right Ascension (J2000)')
+plt.ylabel('Declination (J2000)')
+
+def gaussian(x, y, cx, cy, height, ratio):
+    n = 0.5*np.sqrt(2)
+    G = np.exp(-(x-cx)**2/(n*ratio*height**2) - (y-cy)**2/(n*height**2))
+    return np.ravel(G)
+
+cropim[cropim < 5*np.mean(cropim)] = 0
+p0 = np.array([30, 30, 1, 1])
+popt, pcov = curve_fit(lambda X, cx, cy, height, ratio: gaussian(X[0], X[1], cx, cy, height, ratio), (x, y), np.ravel(cropim), p0)
+fitted = gaussian(x, y, *popt)
+fitted = fitted.reshape(cropim.shape)
+plt.contour(x, y, fitted, 0, colors='red')
+print('Aspect Ratio:', round(popt[3],2))
+
+cbar = plt.colorbar(image, fraction=0.046, pad=0.04)
+cbar.set_label(r'Surface Brightness (mJy arcsec$^{-2}$)', size=12)
+cbar.ax.tick_params(labelsize=11)
